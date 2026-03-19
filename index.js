08fd1632fda5d1f7ba2de4484aa64c7bfae84405
Josiane05 <josianemushimiyimana05@gmail.com>
2026-03-19 15:43:52 +0200

    JavaScript codes

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 console.log("The man whispered,\"please speak to me.\"")
 console.log("up up\n\t down down")
 console.log("Yes" == "yes");
+console.log("green"=="green");
+console.log("green">"blue");
+console.log("Green">"green");
+console.log(0==false);
+console.log("julia" + 1);
