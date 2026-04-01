ab09371374f6d8f98d7645ff37f87f14cc4821a0
Josiane05 <josianemushimiyimana05@gmail.com>
2026-04-01 15:37:10 +0200

    function expression

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-function makeLine(length){
-   var line = "";
-   for(var j = 0; j <= length; j++){
-      line += "*";
+var catSay = function(max){
+   var catMessage = "";
+   for(var i = 0; i < max; i++){
+      catMessage += "meow";
    }
-   return line + "\n";
+   return catMessage;
 }
-function buildTriangle(length) {
-   var triangle = "";
-   var lineNumber = 1;
-   for(var lineNumber = 1; lineNumber <= length; lineNumber++){
-      triangle  = triangle + makeLine(lineNumber);
-   }
-   return triangle;
-}
-console.log(buildTriangle(10));
+console.log(catSay(3));
