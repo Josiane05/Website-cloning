38b0e36004406f4496898d318ce57d549bcf9117
Josiane05 <josianemushimiyimana05@gmail.com>
2026-04-02 16:53:42 +0200

    changing arrays

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-function getCount(str) {
-  var count = 0;
-  var vowels = "aeiou";
-  for(var i = 0; i < str.length; i++ ) {
-      if(vowels.includes(str[i])) {
-         count++;
-      }
-  }
-  return count;
+var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+for (var i = 0; i < prices.length; i++) {
+   if (i === 0 ) {
+      prices[i] = 10;
+   }
+   else if (i === 2) {
+      prices[i] = 20;
+   }
+   else if (i === 6) {
+      prices[i] = 30;
+   }
 }
-console.log(getCount("hello"));
+console.log(prices);
