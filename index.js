a5d5fe3ed46592454932e03d596b18281a07f75d
Josiane05 <josianemushimiyimana05@gmail.com>
2026-04-06 08:48:02 +0200

    push method

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-var donuts = ["glazed", "chocolate", "boston creme", "glazed cruller"];
-donuts.splice(1, 1, "chocolate cruller", "creme de leche");
-console.log(donuts)
+var captain = "Mal";
+var second = "Zoe";
+var pilot = "Wash";
+var companion = "Inara";
+var mercenary = "Jayne";
+var mechanic = "Kaylee";
+
+var crew = [captain, second, pilot, companion, mercenary, mechanic];
+
+var doctor = "Simon";
+var sister = "River";
+var shepherd = "Book";
+crew.push(doctor);
+crew.push(sister);
+crew.push(shepherd)
+console.log(crew);
