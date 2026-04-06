597fa064daa56e4d54728f5389b52b68deb9c9b0
Josiane05 <josianemushimiyimana05@gmail.com>
2026-04-06 08:27:00 +0200

    splice method

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
-for (var i = 0; i < prices.length; i++) {
-   if (i === 0 ) {
-      prices[i] = 10;
-   }
-   else if (i === 2) {
-      prices[i] = 20;
-   }
-   else if (i === 6) {
-      prices[i] = 30;
-   }
-}
-console.log(prices);
+var donuts = ["glazed", "chocolate", "boston creme", "glazed cruller"];
+donuts.splice(1, 1, "chocolate cruller", "creme de leche");
+console.log(donuts)
