0a0da489eb3825d519f8bf0ad72df709b974a2c2
Josiane05 <josianemushimiyimana05@gmail.com>
2026-04-06 15:18:48 +0200

    foreach loop

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var donuts = ["jelly donut", "chocolate donut", "glazed donut"];
-for (var i = 0; i < donuts.length; i++) {
-   donuts[i] += " hole";
-   donuts[i] = donuts[i].toUpperCase();
-   console.log(donuts);
-}
+donuts.forEach(function(donut) {
+  donut += " hole";
+  donut = donut.toUpperCase();
+  console.log(donut);
+});
